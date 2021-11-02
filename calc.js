9db4c9c3bac9ede9a8df17601d4ac13f739b091c
BrunodevOliveira <brunodevoliveira@gmail.com>
2021-11-02 11:08:09 -0300

    Divisão e multiplicação finalizadas

--- calc.js
+++ calc.js
@@ -1,3 +1,20 @@
 function insert(num){
-    document.querySelector('#resultado').innerHTML = num;
+   const concatenar_numero = document.querySelector('#exibir_resultado').innerHTML;
+   document.querySelector('#exibir_resultado').innerHTML = concatenar_numero + num
+}
+function clean(){
+    const limpar_campo = document.querySelector('#exibir_resultado').innerHTML = "";
+    // limpar_campo += "" 
+}
+function back(){
+    const apagar_algarismo = document.querySelector('#exibir_resultado').innerHTML;
+    document.querySelector('#exibir_resultado').innerHTML = apagar_algarismo.substring(0, apagar_algarismo.length -1);
+}
+function calcular(){
+    const exibir_resultado = document.querySelector('#exibir_resultado').innerHTML;
+    
+    // Confirmo se há algum valor para ser calculado. o método eval faz o cálculo
+    if(exibir_resultado){
+        document.querySelector('#exibir_resultado').innerHTML = eval(exibir_resultado);
+    }
 }
